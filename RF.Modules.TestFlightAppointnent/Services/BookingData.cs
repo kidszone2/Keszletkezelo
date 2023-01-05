8db64cd54609e773b547d9a8ce5df3e23019b8a7
VR-DNN\dnn <adam.halassy@stud.uni-corvinus.hu>
2023-01-05 22:54:03 +0100

    Added raw service implementation.

---
+++ RF.Modules.TestFlightAppointnent/Services/BookingData.cs
@@ -0,0 +1,23 @@
+﻿using RF.Modules.TestFlightAppointmentRF.Modules.TestFlightAppointnent.Models;
+using System;
+
+namespace RF.Modules.TestFlightAppointmentRF.Modules.TestFlightAppointnent.Services
+{
+    public class BookingData
+    {
+        public TestFlightBooking Booking { get; }
+
+        public TestFlightParticipant[] Participants { get; }
+
+        internal BookingData(
+            TestFlightBooking booking,
+            TestFlightParticipant[] participants
+            )
+        {
+            Booking = booking 
+                ?? throw new ArgumentNullException(nameof(booking));
+            Participants = participants 
+                ?? throw new ArgumentNullException(nameof(participants));
+        }
+    }
+}
