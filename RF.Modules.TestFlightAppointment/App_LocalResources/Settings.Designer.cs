a800f03ce0c86e26cf49f7c9455d80a0771ed6b1
VR-DNN\dnn <adam.halassy@stud.uni-corvinus.hu>
2023-01-07 17:32:32 +0100

    Template items removed.

--- RF.Modules.TestFlightAppointment/App_LocalResources/Settings.Designer.cs
+++ RF.Modules.TestFlightAppointment/App_LocalResources/Settings.Designer.cs
@@ -39,7 +39,7 @@ namespace RF.Modules.TestFlightAppointmentRF.Modules.TestFlightAppointment.App_L
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RF.Modules.TestFlightAppointmentRF.Modules.TestFlightAppointnent.App_LocalResourc" +
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RF.Modules.TestFlightAppointmentRF.Modules.TestFlightAppointment.App_LocalResourc" +
                             "es.Settings", typeof(Settings).Assembly);
                     resourceMan = temp;
                 }
