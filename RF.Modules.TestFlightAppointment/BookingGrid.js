39739a3f71128e5a4637bc644b6030d6af8d1c5d
VR-DNN\dnn <adam.halassy@stud.uni-corvinus.hu>
2023-01-07 11:39:23 +0100

    Fixed a typo in the project name

---
+++ RF.Modules.TestFlightAppointment/BookingGrid.js
@@ -0,0 +1,3 @@
+function tfBookingGridHelloWorld() {
+    alert("hw");
+}
---
+++ RF.Modules.TestFlightAppointment/Services/ITestFlightBookingManager.cs
@@ -0,0 +1,42 @@
+﻿using RF.Modules.TestFlightAppointmentRF.Modules.TestFlightAppointment.Models;
+using System;
+
+namespace RF.Modules.TestFlightAppointmentRF.Modules.TestFlightAppointment.Services
+{
+    public interface ITestFlightBookingManager
+    {
+        BookingData FindBookingByID(int bookingID);
+
+        TestFlightBooking[] FindBookingsByUser(
+            int userID,
+            DateTime? from,
+            DateTime? to
+            );
+
+        TestFlightBooking[] FindBookingsByDate(
+            DateTime? from,
+            DateTime? to,
+            bool findAll
+            );
+
+        TestFlightBooking CreateBooking(
+            TestFlightBooking booking
+            );
+
+        bool IsSlotAvailable(
+            DateTime from,
+            int duration
+            );
+
+        void CancelBooking(
+            int bookingID
+            );
+
+        TestFlightParticipant AddParticipantTo(
+            int bookingID,
+            TestFlightParticipant participant
+            );
+
+        TestFlightPlan[] FindFlightPlans(bool findAll);
+    }
+}
