7979221aec43a373179503f91ad2d558fd3853e5
VR-DNN\dnn <adam.halassy@stud.uni-corvinus.hu>
2023-01-07 23:24:23 +0100

    API now accessible

---
+++ RF.Modules.TestFlightAppointment/Controllers/Api/PlanController.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+
+namespace RF.Modules.TestFlightAppointment.Controllers.Api
+{
+    public class PlanController : RestApiControllerBase
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public HttpResponseMessage Hello()
+        {
+            return Json(new { message = "Hello, world!" });
+        }
+    }
+}
---
+++ RF.Modules.TestFlightAppointment/Controllers/Api/RestApiControllerBase.cs
@@ -0,0 +1,30 @@
+﻿using DotNetNuke.Web.Api;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+
+namespace RF.Modules.TestFlightAppointment.Controllers.Api
+{
+    public class RestApiControllerBase : DnnApiController
+    {
+        protected HttpResponseMessage Json(object data)
+            => Json(200, data);
+
+        protected HttpResponseMessage Json(HttpStatusCode status, object data)
+        {
+            // Debugger.Launch();
+
+            // var json = JsonConvert.SerializeObject(data); 
+            var response = Request.CreateResponse(
+                status,
+                data,
+                "application/json"
+                );
+            return response;
+        }
+
+        protected HttpResponseMessage Json(int status, object data)
+            => Json((HttpStatusCode)status, data);
+    }
+}
