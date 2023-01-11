3dbbf48fbe4dd7c412b524f4d524d6248dd2f9e1
VR-DNN\dnn <adam.halassy@stud.uni-corvinus.hu>
2023-01-11 22:03:57 +0100

    Minor changes.

--- RF.Modules.TestFlightAppointment/gulpfile.js
+++ RF.Modules.TestFlightAppointment/gulpfile.js
@@ -9,11 +9,13 @@
 
 var gulp = require("gulp");
 var sass = require("gulp-sass")(require('sass'));
+var strip = require("gulp-strip-css-comments");
 
 gulp.task('module-styles', function () {
     return gulp
         .src('Assets/Styles/module.scss')
         .pipe(sass())
+        .pipe(strip())
         .pipe(gulp.dest('./'));
 });
 
