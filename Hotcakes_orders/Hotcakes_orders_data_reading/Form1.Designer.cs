f0c05b8ee21f04fca44dc39ad205f2c31fc487f0
kidszone2 <131201413+kidszone2@users.noreply.github.com>
2023-05-01 11:47:55 +0000

    Projekt kezdete (#1)
    * termékek darabszámának megjelenítése
    
    * összes játék kategória megjelenítése
    
    * Plusz gomb működik
    
    * Validáció elkezdése
    
    ---------
    
    Co-authored-by: mocsi <mocsi@LAPTOP-VH3VH20M>

--- Hotcakes_orders/Hotcakes_orders_data_reading/Form1.Designer.cs
+++ Hotcakes_orders/Hotcakes_orders_data_reading/Form1.Designer.cs
@@ -28,53 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.ordersDataGridView = new System.Windows.Forms.DataGridView();
-            this.startButton = new System.Windows.Forms.Button();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.ordersDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // ordersDataGridView
             // 
+            this.ordersDataGridView.AllowUserToAddRows = false;
+            this.ordersDataGridView.AllowUserToDeleteRows = false;
+            this.ordersDataGridView.AllowUserToOrderColumns = true;
             this.ordersDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.ordersDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.ordersDataGridView.Location = new System.Drawing.Point(24, 24);
             this.ordersDataGridView.Name = "ordersDataGridView";
+            this.ordersDataGridView.ReadOnly = true;
             this.ordersDataGridView.RowHeadersWidth = 51;
             this.ordersDataGridView.RowTemplate.Height = 24;
-            this.ordersDataGridView.Size = new System.Drawing.Size(704, 329);
+            this.ordersDataGridView.Size = new System.Drawing.Size(644, 329);
             this.ordersDataGridView.TabIndex = 0;
             // 
-            // startButton
+            // buttonPlus
             // 
-            this.startButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.startButton.Location = new System.Drawing.Point(24, 385);
-            this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(144, 48);
-            this.startButton.TabIndex = 1;
-            this.startButton.Text = "Start";
-            this.startButton.UseVisualStyleBackColor = true;
-            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            this.buttonPlus.Location = new System.Drawing.Point(868, 117);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.Size = new System.Drawing.Size(60, 59);
+            this.buttonPlus.TabIndex = 1;
+            this.buttonPlus.Text = "+";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.buttonPlus_Click);
+            // 
+            // buttonMinus
+            // 
+            this.buttonMinus.Location = new System.Drawing.Point(868, 197);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new System.Drawing.Size(60, 59);
+            this.buttonMinus.TabIndex = 2;
+            this.buttonMinus.Text = "-";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(714, 166);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(92, 50);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Validating += new System.ComponentModel.CancelEventHandler(this.textBox1_Validating);
+            this.textBox1.Validated += new System.EventHandler(this.textBox1_Validated);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(754, 469);
-            this.Controls.Add(this.startButton);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(999, 469);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonMinus);
+            this.Controls.Add(this.buttonPlus);
             this.Controls.Add(this.ordersDataGridView);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.ordersDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView ordersDataGridView;
-        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
 
--- Hotcakes_orders/Hotcakes_orders_data_reading/Form1.cs
+++ Hotcakes_orders/Hotcakes_orders_data_reading/Form1.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlTypes;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Runtime.Remoting.Proxies;
+using System.Security.Principal;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Hotcakes.CommerceDTO.v1;
+using Hotcakes.CommerceDTO.v1.Catalog;
 using Hotcakes.CommerceDTO.v1.Client;
 using Hotcakes.CommerceDTO.v1.Contacts;
 using Hotcakes.CommerceDTO.v1.Orders;
@@ -20,107 +25,241 @@ namespace Hotcakes_orders_data_reading
 {
     public partial class Form1 : Form
     {
+        List<string> categories = new List<string>();
+        DataTable dt = new DataTable();
+        List<string> bvin = new List<string>();
+        List<int> quantity = new List<int>();
+        List<string> SKU = new List<string>();
+        List<string> product_name = new List<string>();
+
+
         public Form1()
         {
             InitializeComponent();
-            GetOrders();           
+            GetProducts();
+            GetCategories();
+            
         }
 
-        public void ReadData()
+        private void GetCategories()
         {
             string url = "http://20.234.113.211:8090/";
             string key = "1-903011f5-696d-4ed0-9cf8-3a6fa51607f2";
 
             Api proxy = new Api(url, key);
 
-            Random rnd = new Random();
-         
-            using (StreamReader sr = new StreamReader("orders/orders.csv", Encoding.UTF8))
-            {
-                var firstLine = sr.ReadLine();
-
-                while (!sr.EndOfStream) {
-                    
-                    var line = sr.ReadLine();
-                    var item = line.Split(';');
-
-                    // create a new order object
-                    var order = new OrderDTO();
-
-                    // add billing information
-                    order.BillingAddress = new AddressDTO
-                    {
-                        AddressType = AddressTypesDTO.Billing,
-                        City = item[0],
-                        CountryBvin = item[1],
-                        FirstName = item[2],
-                        LastName = item[3],
-                        Line1 = "",
-                        Line2 = "",
-                        Phone = item[4],
-                        PostalCode = item[5],
-                        RegionBvin = item[6]
-                    };
-
-                    // add at least one line item
-                    order.Items = new List<LineItemDTO>();
-                    order.Items.Add(new LineItemDTO
-                    {
-                        ProductId = item[7],
-                        Quantity = int.Parse(item[8])
-                    });
-
-                    // add the shipping address
-                    order.ShippingAddress = new AddressDTO();
-                    order.ShippingAddress = order.BillingAddress;
-                    order.ShippingAddress.AddressType = AddressTypesDTO.Shipping;
-
-                    // specify who is creating the order
-                    order.UserEmail = "info@hotcakescommerce.com";
-                    order.UserID = "1";
-
-                    // call the API to create the order
-                    ApiResponse<OrderDTO> response = proxy.OrdersCreate(order);
-
-                    GetOrders();
-                    Thread.Sleep(rnd.Next(3000, 7000));
-                }
-                
-                sr.Close();
-            }       
-        }
-
-        public void GetOrders()
-        {
-            string url = "http://20.234.113.211:8090/";
-            string key = "1-903011f5-696d-4ed0-9cf8-3a6fa51607f2";
-
-            Api proxy = new Api(url, key);
-
-            // call the API to find all orders in the store
-            ApiResponse<List<OrderSnapshotDTO>> response = proxy.OrdersFindAll();
+            //az össze kategória lekérése
+            ApiResponse<List<CategorySnapshotDTO>> response = proxy.CategoriesFindAll();
             string json = JsonConvert.SerializeObject(response);
 
-            ApiResponse<List<OrderSnapshotDTO>> deserializedResponse = JsonConvert.DeserializeObject<ApiResponse<List<OrderSnapshotDTO>>>(json);
+            ApiResponse<List<CategorySnapshotDTO>> deserializedResponse = JsonConvert.DeserializeObject<ApiResponse<List<CategorySnapshotDTO>>>(json);
 
-            DataTable dt = new DataTable();
-            dt.Columns.Add("Id", typeof(int));
-            dt.Columns.Add("bvin", typeof(string));
-            dt.Columns.Add("FirstName", typeof(string));
-            dt.Columns.Add("LastName", typeof(string));
-            dt.Columns.Add("StoreId", typeof(long));
-
-            foreach (OrderSnapshotDTO item in deserializedResponse.Content)
+            foreach (CategorySnapshotDTO item in deserializedResponse.Content)
             {
-                dt.Rows.Add(item.Id, item.bvin, item.BillingAddress.FirstName, item.BillingAddress.LastName, item.StoreId);
+
+                if (item.ParentId == "aa7af6a8-917e-4e69-8471-33205ded3897")
+                {
+                    categories.Add(item.Name);
+                }
+            }
+
+            for (int i = 0; i < categories.Count; i++)
+            {
+                Button button = new Button();
+                button.Text = categories[i];
+                Left = i * 100;
+                Width = 100;
+                
+                Controls.Add(button);
+            } 
+        }
+
+        public void GetProducts()
+        {
+            string url = "http://20.234.113.211:8090/";
+            string key = "1-903011f5-696d-4ed0-9cf8-3a6fa51607f2";
+
+            Api proxy = new Api(url, key);
+
+            //az összes termék lekérése
+            ApiResponse<List<ProductDTO>> response = proxy.ProductsFindAll();
+            string json = JsonConvert.SerializeObject(response);
+
+            ApiResponse<List<ProductDTO>> deserializedResponse = JsonConvert.DeserializeObject<ApiResponse<List<ProductDTO>>>(json);
+
+            foreach (ProductDTO item in deserializedResponse.Content)
+            {
+                bvin.Add(item.Bvin);
+                SKU.Add(item.Sku);
+                product_name.Add(item.ProductName);
+            }
+
+            //termékek darabszámának lekérése
+            for (int i = 0; i < bvin.Count; i++)
+            {
+                string aktBvin = bvin[i];
+                ApiResponse<List<ProductInventoryDTO>> response_v2 = proxy.ProductInventoryFindForProduct(aktBvin);
+                string json_v2 = JsonConvert.SerializeObject(response_v2);
+
+                ApiResponse<List<ProductInventoryDTO>> deserializedResponse_v2 = JsonConvert.DeserializeObject<ApiResponse<List<ProductInventoryDTO>>>(json_v2);
+
+                foreach (ProductInventoryDTO item in deserializedResponse_v2.Content)
+                {
+                    quantity.Add(item.QuantityOnHand);
+                }
+            }
+
+            dt.Columns.Add("SKU", typeof(string));
+            dt.Columns.Add("Terméknév", typeof(string));
+            dt.Columns.Add("Mennyiség", typeof(int));
+
+            for (int i = 0; i < bvin.Count; i++)
+            {
+                dt.Rows.Add(SKU[i], product_name[i], quantity[i]);
             }
 
             ordersDataGridView.DataSource = dt;
         }
 
-        private void startButton_Click(object sender, EventArgs e)
+        private void buttonPlus_Click(object sender, EventArgs e)
         {
-            ReadData();
+            int rowIndex = ordersDataGridView.CurrentCell.RowIndex;
+
+            dt.Rows[rowIndex].SetField("Mennyiség", dt.Rows[rowIndex].Field<int>("Mennyiség") + int.Parse(textBox1.Text));
+        }
+
+        private void textBox1_Validated(object sender, EventArgs e)
+        {
+            errorProvider1.SetError(textBox1, "");
+        }
+
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                errorProvider1.SetError(textBox1, "Az érték nem lehet üres");
+            }    
         }
     }
 }
+
+
+//foreach (var item in dt.AsEnumerable())
+//{
+
+//    if (item.Field<string>("SKU") == SKU[rowIndex])
+//    {
+//        MessageBox.Show(item.Field<string>("Terméknév").ToString());
+//        item.Field<int>("Mennyiség") = 5 ;
+//    }
+
+//}
+
+//namespace Hotcakes_orders_data_reading
+//{
+//    public partial class Form1 : Form
+//    {
+//        public Form1()
+//        {
+//            InitializeComponent();
+//            GetOrders();           
+//        }
+
+//        public void ReadData()
+//        {
+//            string url = "http://20.234.113.211:8090/";
+//            string key = "1-903011f5-696d-4ed0-9cf8-3a6fa51607f2";
+
+//            Api proxy = new Api(url, key);
+
+//            Random rnd = new Random();
+
+//            using (StreamReader sr = new StreamReader("orders/orders.csv", Encoding.UTF8))
+//            {
+//                var firstLine = sr.ReadLine();
+
+//                while (!sr.EndOfStream) {
+
+//                    var line = sr.ReadLine();
+//                    var item = line.Split(';');
+
+//                    // create a new order object
+//                    var order = new OrderDTO();
+
+//                    // add billing information
+//                    order.BillingAddress = new AddressDTO
+//                    {
+//                        AddressType = AddressTypesDTO.Billing,
+//                        City = item[0],
+//                        CountryBvin = item[1],
+//                        FirstName = item[2],
+//                        LastName = item[3],
+//                        Line1 = "",
+//                        Line2 = "",
+//                        Phone = item[4],
+//                        PostalCode = item[5],
+//                        RegionBvin = item[6]
+//                    };
+
+//                    // add at least one line item
+//                    order.Items = new List<LineItemDTO>();
+//                    order.Items.Add(new LineItemDTO
+//                    {
+//                        ProductId = item[7],
+//                        Quantity = int.Parse(item[8])
+//                    });
+
+//                    // add the shipping address
+//                    order.ShippingAddress = new AddressDTO();
+//                    order.ShippingAddress = order.BillingAddress;
+//                    order.ShippingAddress.AddressType = AddressTypesDTO.Shipping;
+
+//                    // specify who is creating the order
+//                    order.UserEmail = "info@hotcakescommerce.com";
+//                    order.UserID = "1";
+
+//                    // call the API to create the order
+//                    ApiResponse<OrderDTO> response = proxy.OrdersCreate(order);
+
+//                    GetOrders();
+//                    Thread.Sleep(rnd.Next(3000, 7000));
+//                }
+
+//                sr.Close();
+//            }       
+//        }
+
+//        public void GetOrders()
+//        {
+//            string url = "http://20.234.113.211:8090/";
+//            string key = "1-903011f5-696d-4ed0-9cf8-3a6fa51607f2";
+
+//            Api proxy = new Api(url, key);
+
+//            // call the API to find all orders in the store
+//            ApiResponse<List<OrderSnapshotDTO>> response = proxy.OrdersFindAll();
+//            string json = JsonConvert.SerializeObject(response);
+
+//            ApiResponse<List<OrderSnapshotDTO>> deserializedResponse = JsonConvert.DeserializeObject<ApiResponse<List<OrderSnapshotDTO>>>(json);
+
+//            DataTable dt = new DataTable();
+//            dt.Columns.Add("Id", typeof(int));
+//            dt.Columns.Add("bvin", typeof(string));
+//            dt.Columns.Add("FirstName", typeof(string));
+//            dt.Columns.Add("LastName", typeof(string));
+//            dt.Columns.Add("StoreId", typeof(long));
+
+//            foreach (OrderSnapshotDTO item in deserializedResponse.Content)
+//            {
+//                dt.Rows.Add(item.Id, item.bvin, item.BillingAddress.FirstName, item.BillingAddress.LastName, item.StoreId);
+//            }
+
+//            ordersDataGridView.DataSource = dt;
+//        }
+
+//        private void startButton_Click(object sender, EventArgs e)
+//        {
+//            ReadData();
+//        }
+//    }
+//}
